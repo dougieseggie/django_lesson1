b811497c167737e6f413aa61ddac9e4c42f47dfd
Hyunduk Kim <slakingex@naver.com>
2019-08-31 23:51:04 +0900

    08/31(2)

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.http import Http404
 from .models import Board
 from .forms import BoardForm
 from fcuser.models import Fcuser
@@ -6,17 +8,30 @@ from fcuser.models import Fcuser
 
 
 def board_list(request):
-    boards = Board.objects.all().order_by('-id')
+    all_boards = Board.objects.all().order_by('-id')
     # all 은 다 가져오는 함수 / -id의 -는 역순이란 뜻
+
+    page = request.GET.get('p', 1)
+    # p라는 값으로 받고 없으면 첫 번째 페이지로
+    paginator = Paginator(all_boards, 2)
+    # 한 페이지 당 몇 개씩 보여줄건지
+    boards = paginator.get_page(page)
+    # 앞서 설정한 page 값을 paginator 에 넣어서 보여줌
     return render(request, 'board_list.html', {'boards': boards})
 
 
 def board_detail(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다')
+
     return render(request, 'board_detail.html', {'board': board})
 
 
 def board_write(request):
+    if not request.session.get('user'):
+        return redirect('/fcuser/login/')
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
